e3ad4a0f09e2329441582d8ef75f1576b8b67c5f
DiegoChevez <diegochevez144@gmail.com>
2022-10-26 16:39:36 -0600

    creacion de mapa Âparte 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //Boton Hiddent
 document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
 
@@ -17,3 +18,42 @@ function ocultar_menu(){
     nav.style.right = "-250px";
     background_menu.style.display = "none";
 }
+
+
+
+function mostrarAhuachapan(){
+    const $CardAhuachapan = document.querySelector(".Ahuachapan");
+    $CardAhuachapan.style.setProperty("visibility","visible");
+}
+function ocultarAhuachapan(){
+    const $CardAhuachapan = document.querySelector(".Ahuachapan");
+    $CardAhuachapan.style.setProperty("visibility","hidden");
+}
+
+function mostrarCabañas(){
+    const $CardCabañas = document.querySelector(".Cabañas");
+    $CardCabañas.style.setProperty("visibility","visible");
+}
+function ocultarCabañas(){
+    const $CardCabañas = document.querySelector(".Cabañas");
+    $CardCabañas.style.setProperty("visibility","hidden");
+}
+
+function mostrarCuscatlán(){
+    const $CardCuscatlán = document.querySelector(".Cuscatlán");
+    $CardCuscatlán.style.setProperty("visibility","visible");
+}
+function ocultarCuscatlán(){
+    const $CardCuscatlán = document.querySelector(".Cuscatlán");
+    $CardCuscatlán.style.setProperty("visibility","hidden");
+}
+
+function mostrarLibertad(){
+    const $CardLibertad = document.querySelector(".Libertad");
+    $CardLibertad.style.setProperty("visibility","visible");
+}
+function ocultarLibertad(){
+    const $CardLibertad = document.querySelector(".Libertad");
+    $CardLibertad.style.setProperty("visibility","hidden");
+}
+
